0d7868a47c4c247b058f26465c8cdec5718a9fd9
tayueliuxiang1@sina.com <tayueliuxiang1@sina.com>
2020-03-31 14:28:06 +0800

    request response controller init

--- src/Framework/SwServer/ServerController.php
+++ src/Framework/SwServer/ServerController.php
@@ -7,27 +7,31 @@
  */
 
 namespace Framework\SwServer;
+
 use Framework\SwServer\Base\BaseObject;
 use Framework\SwServer\Http\HttpInput;
-use Framework\SwServer\ServerManager;
 use Framework\SwServer\Http\HttpOutput;
 
 class ServerController extends BaseObject
 {
-    protected $httpInput=null;
-    protected $httpOutput=null;
+    protected $httpInput = null;
+    protected $httpOutput = null;
+    protected $request;
+    protected $response;
 
 
     public function init()
     {
-        $this->httpInput=new HttpInput(ServerManager::getApp()->request);
-        $this->httpOutput=new HttpOutput(ServerManager::getApp()->request,ServerManager::getApp()->response);
+        $this->httpInput = new HttpInput(ServerManager::getApp()->request);
+        $this->request = ServerManager::getApp()->request;
+        $this->response = ServerManager::getApp()->response;
+        $this->httpOutput = new HttpOutput(ServerManager::getApp()->request, ServerManager::getApp()->response);
     }
 
-    public function __call($name, $arguments='')
+    public function __call($name, $arguments = '')
     {
         // TODO: Implement __call() method.
-        echo __CLASS__."方法{$name}不存在!!";
+        echo __CLASS__ . "方法{$name}不存在!!";
     }
 
 
